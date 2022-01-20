8b6034a4bff5e28181a2253b1c8483eeb89bd064
Chris <chirssel@gmail.com>
2022-01-20 20:04:19 +0100

    Properly layout different previews on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import { Box, ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider, Wrap } from '@chakra-ui/react';
 import Discord from './components/demo/Discord';
 
 const App: React.FC = () => {
 	return (
 		<ChakraProvider>
-			<Box p={8} minH="100vh" bg="gray.50">
-				<Discord
-					meta={{
-						tags: {
-							title: 'title',
-							description: 'description',
-							'og:image': 'https://via.placeholder.com/800x450',
-						},
-					}}
-				/>
-			</Box>
+			<Wrap p={8} minH="100vh" bg="gray.50" spacing={8} justify="center">
+				<Box flex={1} minW="md" maxW="xl">
+					<Discord
+						meta={{
+							tags: {
+								title: 'title',
+								description: 'description',
+								'og:image': 'https://via.placeholder.com/800x450',
+							},
+						}}
+					/>
+				</Box>
+				<Box flex={1} minW="md" maxW="xl">
+					<Discord
+						meta={{
+							tags: {
+								title: 'title',
+								description: 'description',
+								'og:image': 'https://via.placeholder.com/800x450',
+							},
+						}}
+					/>
+				</Box>
+			</Wrap>
 		</ChakraProvider>
 	);
 };
--- src/components/PreviewFrame.tsx
+++ src/components/PreviewFrame.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from '@chakra-ui/react';
 
 const PreviewFrame: React.FC<{ title: string; icon?: React.ReactNode }> = (props) => {
 	return (
-		<Box borderWidth="1px" borderColor="gray.100" bg="white" maxW="xl" boxShadow="lg">
+		<Box borderWidth="1px" borderColor="gray.100" bg="white" boxShadow="lg">
 			<Box px={4} py={2}>
 				<Text objectPosition="middle" fontWeight="600">
 					{props.icon}
