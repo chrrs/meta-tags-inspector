cae219ea7fc810b0e73492aadbbf4ad2c2f23f2c
Chris <chirssel@gmail.com>
2022-01-20 20:29:36 +0100

    Add a raw tags preview panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
 import { Box, ChakraProvider, Wrap } from '@chakra-ui/react';
 import Discord from './components/preview/Discord';
+import RawTags from './components/preview/RawTags';
 
 const App: React.FC = () => {
+	const meta = {
+		tags: {
+			title: 'Some website',
+			description:
+				'This is a pretty long description in order to test how the UI will look when lines overflow onto the next line.',
+			'og:image': 'https://via.placeholder.com/800x450',
+			'og:site_name': 'Site',
+			'twitter:card': 'summary_large_image',
+		},
+	};
+
 	return (
 		<ChakraProvider>
 			<Wrap p={8} minH="100vh" bg="gray.50" spacing={8} justify="center">
 				<Box flex={1} minW="md" maxW="xl">
-					<Discord
-						meta={{
-							tags: {
-								title: 'title',
-								description: 'description',
-								'og:image': 'https://via.placeholder.com/800x450',
-							},
-						}}
-					/>
+					<RawTags meta={meta} />
 				</Box>
 				<Box flex={1} minW="md" maxW="xl">
-					<Discord
-						meta={{
-							tags: {
-								title: 'title',
-								description: 'description',
-								'og:image': 'https://via.placeholder.com/800x450',
-							},
-						}}
-					/>
+					<Discord meta={meta} />
 				</Box>
 			</Wrap>
 		</ChakraProvider>
---
+++ src/components/preview/RawTags.tsx
@@ -0,0 +1,54 @@
+import { Box, VStack, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Meta } from '../../lib/meta';
+import PreviewFrame from '../PreviewFrame';
+
+const RawTags: React.FC<{ meta: Meta }> = (props) => {
+	// TODO: Add more specific information to each property. (enum values, image links, etc.)
+
+	const categories = [
+		['OpenGraph', 'og:'],
+		['Twitter', 'twitter:'],
+	];
+
+	const [tags, setTags] = useState<Record<string, [string, string][]>>({});
+
+	useEffect(() => {
+		const tags: Record<string, [string, string][]> = {};
+
+		for (const [tag, value] of Object.entries(props.meta.tags)) {
+			const category = categories.find((e) => tag.startsWith(e[1]))?.[0] ?? 'General';
+			const list = tags[category] ?? [];
+			list.push([tag, value]);
+			tags[category] = list;
+		}
+
+		setTags(tags);
+	}, [props.meta]);
+
+	return (
+		<PreviewFrame title="Raw tags">
+			<VStack px={4} py={3} bg="gray.50" spacing={4} align="start">
+				{['General', ...categories.map((it) => it[0])].map(
+					(category) =>
+						tags[category] && (
+							<Box w="full">
+								<Text fontSize="sm" fontWeight="600" color="gray.500" mb={1}>
+									{category}
+								</Text>
+								<VStack lineHeight="5" spacing={1} align="start">
+									{tags[category]?.map((item) => (
+										<Text>
+											<Text as="code">{item[0]}</Text>: {item[1]}
+										</Text>
+									))}
+								</VStack>
+							</Box>
+						)
+				)}
+			</VStack>
+		</PreviewFrame>
+	);
+};
+
+export default RawTags;
