0483b928d569c7d5d9eb1542e73600b15bab8b71
Chris <chirssel@gmail.com>
2022-02-17 00:04:19 +0100

    Change Meta to a class

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -1,18 +1,41 @@
-export interface Meta {
-	tags: Record<string, string>;
+export class Meta {
+	tags: Record<string, string> = {};
+
+	get(name: string | string[]): string | undefined {
+		if (typeof name === 'string') {
+			return this.tags[name];
+		} else {
+			for (const s of name) {
+				const tag = this.tags[s];
+				if (tag) {
+					return tag;
+				}
+			}
+
+			return undefined;
+		}
+	}
+
+	update(tags: Record<string, string>) {
+		this.tags = tags;
+	}
+
+	subset<S extends readonly string[]>(subset: S) {
+		return new MetaSubset(this, subset);
+	}
 }
 
-export function getTag(meta: Meta, name: string | string[]): string | undefined {
-	if (typeof name === 'string') {
-		return meta.tags[name];
-	} else {
-		for (const s of name) {
-			const tag = meta.tags[s];
-			if (tag) {
-				return tag;
-			}
-		}
+export class MetaSubset<S extends readonly string[], T = keyof S> {
+	meta: Meta;
+	subset: S;
 
-		return undefined;
+	constructor(meta: Meta, subset: S) {
+		this.meta = meta;
+		this.subset = subset;
+	}
+
+	get(name: T | T[]): string | undefined {
+		// @ts-ignore
+		return this.meta.get(name);
 	}
 }
