75928b48cbf9af74c0363a19c29f9f4c54f86338
Chris <chirssel@gmail.com>
2022-02-17 12:42:58 +0100

    Avoid putting the Meta class inside a React state

--- src/devtools/App.tsx
+++ src/devtools/App.tsx
@@ -1,28 +1,24 @@
-import { Meta } from '$lib/meta';
-import { useEffect, useReducer } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 const App: React.FC = () => {
-	// TODO: useState tags
-	const [_, forceUpdate] = useReducer((x) => x + 1, 0);
-	const [meta] = useState(new Meta());
+	const [tags, setTags] = useState<Record<string, string>>({});
 
 	useEffect(() => {
 		const port = chrome.runtime.connect();
 
 		port.onMessage.addListener((message: [string, any]) => {
-			meta.update(message[1] ?? {});
-			forceUpdate();
+			setTags(message[1]);
 		});
 
 		port.postMessage(['init', { tabId: chrome?.devtools?.inspectedWindow?.tabId }]);
 
 		return () => port.disconnect();
-	}, [meta]);
+	}, []);
 
 	return (
 		<>
-			{Object.entries(meta.tags).map(([key, value]) => (
+			{Object.entries(tags).map(([key, value]) => (
 				<p key={key}>
 					<b>{key}:</b> {value}
 				</p>
