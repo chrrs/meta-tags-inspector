a305ea55048d5365f5c9ba652cac87cf1a11dc37
Chris <chirssel@gmail.com>
2022-02-19 17:09:19 +0100

    Add a supported tags popup

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,4 +1,5 @@
 import { MetaSubset } from '$lib/meta';
+import { InfoOutlineIcon } from '@chakra-ui/icons';
 import { Box, Code, Flex, Text, Tooltip } from '@chakra-ui/react';
 
 const Preview: React.FC<{
@@ -7,8 +8,27 @@ const Preview: React.FC<{
 }> = ({ subset, title, children }) => {
 	return (
 		<Box>
-			<Flex justify="space-between" px={4} py={2} bg="gray.100">
+			<Flex justify="space-between" align="center" px={4} py={2} bg="gray.100">
 				<Text fontWeight="bold">{title}</Text>
+				<Tooltip
+					bg="white"
+					color="black"
+					label={
+						<Flex direction="column" align="flex-start" gap={1} p={2}>
+							<Text fontWeight="bold">Supported meta tags</Text>
+							{subset.subset.map((tag: string) => (
+								<Code
+									key={tag}
+									colorScheme={subset.get(tag) === undefined ? 'red' : 'green'}
+								>
+									{tag}
+								</Code>
+							))}
+						</Flex>
+					}
+				>
+					<InfoOutlineIcon />
+				</Tooltip>
 			</Flex>
 			{children}
 		</Box>
