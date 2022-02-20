ebcd5e6123cf872d51fc9c8961a4426b1f97a25f
Chris <chirssel@gmail.com>
2022-02-20 13:45:17 +0100

    Add the possibility of showing potential issues for previews

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,34 +1,60 @@
 import { MetaSubset } from '$lib/meta';
-import { InfoOutlineIcon } from '@chakra-ui/icons';
-import { Box, Code, Flex, Text, Tooltip } from '@chakra-ui/react';
+import { InfoOutlineIcon, WarningTwoIcon } from '@chakra-ui/icons';
+import { Box, Code, Flex, HStack, ListItem, Text, Tooltip, UnorderedList } from '@chakra-ui/react';
 
 const Preview: React.FC<{
 	subset: MetaSubset<any>;
 	title: string;
-}> = ({ subset, title, children }) => {
+	issues?: string[];
+}> = ({ subset, title, children, issues }) => {
 	return (
 		<Box>
 			<Flex justify="space-between" align="center" px={4} py={2} bg="gray.100">
 				<Text fontWeight="bold">{title}</Text>
-				<Tooltip
-					bg="white"
-					color="black"
-					label={
-						<Flex direction="column" align="flex-start" gap={1} p={2}>
-							<Text fontWeight="bold">Supported meta tags</Text>
-							{subset.subset.map((tag: string) => (
-								<Code
-									key={tag}
-									colorScheme={subset.get(tag) === undefined ? 'red' : 'green'}
-								>
-									{tag}
-								</Code>
-							))}
-						</Flex>
-					}
-				>
-					<InfoOutlineIcon />
-				</Tooltip>
+				<HStack gap={4}>
+					{issues && issues.length > 0 && (
+						<Tooltip
+							bg="white"
+							color="black"
+							label={
+								<Box p={2}>
+									<Text fontWeight="bold">Potential issues</Text>
+									<UnorderedList fontWeight="normal" color="yellow.500">
+										{issues.map((issue: string) => (
+											<ListItem key={issue}>{issue}</ListItem>
+										))}
+									</UnorderedList>
+								</Box>
+							}
+						>
+							<Text color="yellow.500">
+								<WarningTwoIcon mb={1} mr={0.5} /> {issues.length} potential issue
+								{issues.length != 1 ? 's' : ''} found
+							</Text>
+						</Tooltip>
+					)}
+					<Tooltip
+						bg="white"
+						color="black"
+						label={
+							<Flex direction="column" align="flex-start" gap={1} p={2}>
+								<Text fontWeight="bold">Supported meta tags</Text>
+								{subset.subset.map((tag: string) => (
+									<Code
+										key={tag}
+										colorScheme={
+											subset.get(tag) === undefined ? 'red' : 'green'
+										}
+									>
+										{tag}
+									</Code>
+								))}
+							</Flex>
+						}
+					>
+						<InfoOutlineIcon />
+					</Tooltip>
+				</HStack>
 			</Flex>
 			{children}
 		</Box>
