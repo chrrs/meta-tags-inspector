5b140b87ebeca87face8afa64f6c108b5d7507f0
Chris <chirssel@gmail.com>
2022-03-19 17:26:02 +0100

    Add a fetching and error overlay

--- src/devtools/App.tsx
+++ src/devtools/App.tsx
@@ -2,7 +2,7 @@ import DiscordPreview from '$components/preview/DiscordPreview';
 import { TwitterPreview } from '$components/preview/TwitterPreview';
 import { Meta } from '$lib/meta';
 import { RepeatIcon } from '@chakra-ui/icons';
-import { Box, Button, ChakraProvider, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, ChakraProvider, Flex, Spinner, Text } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
@@ -10,10 +10,12 @@ let port: chrome.runtime.Port | null = null;
 
 const App: React.VFC = () => {
 	const [fetching, setFetching] = useState(true);
+	const [error, setError] = useState<string | null>(null);
 	const [tags, setTags] = useState<Record<string, string>>({});
 
 	function refetch() {
 		setFetching(true);
+		setError(null);
 		port?.postMessage(['refetch']);
 	}
 
@@ -27,15 +29,19 @@ const App: React.VFC = () => {
 				console.log('received', message);
 				if (message[0] === 'fetch:url') {
 					setFetching(true);
+					setError(null);
 
 					const url = message[1] as string;
 					setTags({ '<url>': url });
 				} else if (message[0] === 'fetch:error') {
 					setFetching(false);
-					// TODO: Do something
+					setError(message[1] as string);
 				} else if (message[0] === 'fetch:tags') {
 					setFetching(false);
-					setTags(message[1] as Record<string, string>);
+
+					const tags = message[1] as Record<string, string>;
+					setTags(tags);
+					console.log(tags);
 				}
 			});
 
@@ -47,7 +53,7 @@ const App: React.VFC = () => {
 
 	return (
 		<ChakraProvider>
-			<Box fontSize="sm">
+			<Flex w="100vw" h="100vh" direction="column" fontSize="sm">
 				<Flex px={4} py={2} gap={2} bg="gray.100" align="center" justify="space-between">
 					<Text>
 						<b>Note: </b>
@@ -67,16 +73,54 @@ const App: React.VFC = () => {
 						Refetch tags
 					</Button>
 				</Flex>
-				<Flex p={4} gap={4} direction={{ base: 'column', xl: 'row' }}>
-					<Box flex="1">
-						<DiscordPreview meta={new Meta(tags)} />
-					</Box>
-					<Box flex="1">
-						<TwitterPreview meta={new Meta(tags)} />
-					</Box>
-				</Flex>
-				{JSON.stringify(tags)}
-			</Box>
+				<Box
+					flex={1}
+					/* TODO: This could cause some things to go offscreen */
+					overflowX="hidden"
+					/* TODO: This causes the error message to sometimes be offscreen, because scroll isn't reset */
+					overflowY={fetching || error ? 'hidden' : 'auto'}
+					position="relative"
+				>
+					<Flex
+						position="absolute"
+						top={0}
+						left={0}
+						right={0}
+						p={4}
+						gap={4}
+						direction={{ base: 'column', xl: 'row' }}
+					>
+						<Box flex="1">
+							<DiscordPreview meta={new Meta(tags)} />
+						</Box>
+						<Box flex="1">
+							<TwitterPreview meta={new Meta(tags)} />
+						</Box>
+					</Flex>
+					{(fetching || error) && (
+						<Flex
+							position="absolute"
+							top={0}
+							bottom={0}
+							left={0}
+							right={0}
+							align="center"
+							justify="center"
+							bg="whiteAlpha.800"
+						>
+							{fetching && <Spinner />}
+							{error && (
+								<Box textAlign="center">
+									<Text as="h1" fontWeight="semibold" fontSize={24}>
+										Can't inspect page.
+									</Text>
+									<Text mt={1}>{error}</Text>
+								</Box>
+							)}
+						</Flex>
+					)}
+				</Box>
+			</Flex>
 		</ChakraProvider>
 	);
 };
